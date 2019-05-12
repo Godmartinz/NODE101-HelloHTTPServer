cc200f88da5daab92e098e8ab7675581177d33cc
Godfrey Martinez <godmartinz@gmail.com>
2019-05-12 11:00:07 -0700

    adds initial project files

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code here
+var http=require('http');
+
+http.createServer(function(req, res){
+    res.writeHead(200,{'Content-type':'text/event-stream'});
+    res.write('Hello World');
+    res.end();
+        
+    }).listen(8080);
+console.log("listening on port: ");
